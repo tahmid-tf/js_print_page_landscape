6b14487a4acb10f6679e365d63f71e04d1cf6418
Tahmid Ferdous <62140352+tahmid-tf@users.noreply.github.com>
2021-12-24 00:26:35 +0600

    print page in landscape format

---
+++ landscape.js
@@ -0,0 +1,19 @@
+<script>
+    var css = '@page { size: landscape; margin: 0}',
+    head = document.head || document.getElementsByTagName('head')[0],
+    style = document.createElement('style');
+
+    style.type = 'text/css';
+    style.media = 'print';
+
+    if (style.styleSheet){
+    style.styleSheet.cssText = css;
+    } else {
+    style.appendChild(document.createTextNode(css));
+    }
+
+    head.appendChild(style);
+
+    window.print();
+
+</script>
